ee8e754b85166f31529594ae8eace9585f8cbec5
Saman Batool <samanbatool08@gmail.com>
2022-09-30 13:01:33 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Stock.create([
+  {ticker: 'MSFT', name: "Microsoft", price: "148.00"}
+  {ticker: 'APP', name: "Apple", price: "141.44"}
+  {ticker: 'GOOG', name: "Google", price: "97.49"}
+  {ticker: 'AMZN', name: "Amazon", price: "115.80"}
+  {ticker: 'TSLA', name: "Tesla", price: "271.13"}
+  {ticker: 'BRK', name: "Berkshire Hathaway", price: "270.39"}
+  {ticker: 'UNH', name: "United Health Group", price: "511.94"}
+  {ticker: 'JNJ', name: "Johnson and Johnson", price: "164.96"}
+  {ticker: 'V', name: "Visa Inc", price: "180.52"}
+  {ticker: 'META', name: "Meta Platforms", price: "138.76"}
+  {ticker: 'XOM', name: "Exxon Mobile", price: "88.37"}
+  {ticker: 'TSM', name: "Taiwan Semiconductor Manufacturing Company", price: "69.16"}
+  {ticker: 'WMT', name: "Walmart", price: "131.40"}
+  {ticker: 'JPM', name: "JP Morgan Chase", price: "105.63"}
+  {ticker: 'LLY', name: "Eli Lilly and Company", price: "326.28"}
+  {ticker: 'NVDA', name: "NVIDIA Corporation", price: "124.38"}
+  {ticker: 'PG', name: "Proctor & Gamble Company", price: "127.77"}
+  {ticker: 'HD', name: "Home Depot Inc", price: "280.43"}
+  {ticker: 'CVX', name: "Chervon Corporation", price: "144.39"}
+  {ticker: 'MA', name: "MasterCard Incorporated", price: "288.65"}
+  {ticker: 'PFE', name: "Pfizer Inc", price: "44.45"}
+  {ticker: 'BAC', name: "Bank Of America", price: "30.57"}
+  {ticker: 'KO', name: "Coca-Cola Company", price: "56.41"}
+  {ticker: 'PEP', name: "PepsiCo Inc", price: "165.73"}
+  {ticker: 'NVO', name: "Novo Nordisk", price: "100.22"}
+  {ticker: 'MRK', name: "Merck & Company Inc", price: "86.86"}
+  {ticker: 'BABA', name: "Alibaba Group Holding", price: "80.90"}
+  {ticker: 'COST', name: "Costco", price: "480.65"}
+  {ticker: 'TMO', name: "Thermo Fisher", price: "516.89"}
+  {ticker: 'DHR', name: "Danaher Corporation", price: "262.46"}
+  {ticker: 'AVGO', name: "Broadcom Inc", price: "451.99"}
+  {ticker: 'TM', name: "Toyota Motor", price: "131.23"}
+  {ticker: 'SHEL', name: "Royal Dutch Shell", price: "49.81"}
+  {ticker: 'DIS', name: "Walt Disney Comp", price: "96.42"}
+  {ticker: 'ABT', name: "Abott Lab", price: "98.79"}
+  {ticker: 'MCD', name: "McDonalds Corporation", price: "232.28"}
+  {ticker: 'AZN', name: "AstraZeneca PLC", price: "55.10"}
+  {ticker: 'TMUS', name: "T-Mobile US", price: "135.05"}
+  {ticker: 'CSCO', name: "Cisco Systems Inc", price: "40.56"}
+  {ticker: 'BMY', name: "Bristol-Meyers Squibb", price: "71.92"}
+])
