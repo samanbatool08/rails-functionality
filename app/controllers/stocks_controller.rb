542e2b454b17e11f1abdeb82269bef41494478b3
Saman Batool <samanbatool08@gmail.com>
2022-10-01 13:22:59 -0400

    handle invalid stock searches

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,19 @@
 class StocksController < ActionController::Base
   
   def search
-    stock = Stock.where(ticker: params[:stock])
-    render json: stock 
+    if params[:stock].present? 
+      @stock = Stock.where(ticker: params[:stock]).first
+      if @stock 
+        render 'users/my_portfolio'
+      else 
+        flash[:alert] = "Please enter a valid symbol to search" 
+        redirect_to my_portfolio_path
+      end 
+    else 
+      flash[:alert] = "Please enter a symbol to search"
+      redirect_to my_portfolio_path
+    end
   end 
+   
+
 end
