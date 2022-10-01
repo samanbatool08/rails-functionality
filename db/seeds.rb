89021e974ad5de41ec3ce3df98ee18214ab2d38f
Saman Batool <samanbatool08@gmail.com>
2022-09-30 20:16:00 -0400

    setup frontend structure for stock lookup

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,44 +8,44 @@
 
 
 Stock.create([
-  {ticker: 'MSFT', name: "Microsoft", price: "148.00"}
-  {ticker: 'APP', name: "Apple", price: "141.44"}
-  {ticker: 'GOOG', name: "Google", price: "97.49"}
-  {ticker: 'AMZN', name: "Amazon", price: "115.80"}
-  {ticker: 'TSLA', name: "Tesla", price: "271.13"}
-  {ticker: 'BRK', name: "Berkshire Hathaway", price: "270.39"}
-  {ticker: 'UNH', name: "United Health Group", price: "511.94"}
-  {ticker: 'JNJ', name: "Johnson and Johnson", price: "164.96"}
-  {ticker: 'V', name: "Visa Inc", price: "180.52"}
-  {ticker: 'META', name: "Meta Platforms", price: "138.76"}
-  {ticker: 'XOM', name: "Exxon Mobile", price: "88.37"}
-  {ticker: 'TSM', name: "Taiwan Semiconductor Manufacturing Company", price: "69.16"}
-  {ticker: 'WMT', name: "Walmart", price: "131.40"}
-  {ticker: 'JPM', name: "JP Morgan Chase", price: "105.63"}
-  {ticker: 'LLY', name: "Eli Lilly and Company", price: "326.28"}
-  {ticker: 'NVDA', name: "NVIDIA Corporation", price: "124.38"}
-  {ticker: 'PG', name: "Proctor & Gamble Company", price: "127.77"}
-  {ticker: 'HD', name: "Home Depot Inc", price: "280.43"}
-  {ticker: 'CVX', name: "Chervon Corporation", price: "144.39"}
-  {ticker: 'MA', name: "MasterCard Incorporated", price: "288.65"}
-  {ticker: 'PFE', name: "Pfizer Inc", price: "44.45"}
-  {ticker: 'BAC', name: "Bank Of America", price: "30.57"}
-  {ticker: 'KO', name: "Coca-Cola Company", price: "56.41"}
-  {ticker: 'PEP', name: "PepsiCo Inc", price: "165.73"}
-  {ticker: 'NVO', name: "Novo Nordisk", price: "100.22"}
-  {ticker: 'MRK', name: "Merck & Company Inc", price: "86.86"}
-  {ticker: 'BABA', name: "Alibaba Group Holding", price: "80.90"}
-  {ticker: 'COST', name: "Costco", price: "480.65"}
-  {ticker: 'TMO', name: "Thermo Fisher", price: "516.89"}
-  {ticker: 'DHR', name: "Danaher Corporation", price: "262.46"}
-  {ticker: 'AVGO', name: "Broadcom Inc", price: "451.99"}
-  {ticker: 'TM', name: "Toyota Motor", price: "131.23"}
-  {ticker: 'SHEL', name: "Royal Dutch Shell", price: "49.81"}
-  {ticker: 'DIS', name: "Walt Disney Comp", price: "96.42"}
-  {ticker: 'ABT', name: "Abott Lab", price: "98.79"}
-  {ticker: 'MCD', name: "McDonalds Corporation", price: "232.28"}
-  {ticker: 'AZN', name: "AstraZeneca PLC", price: "55.10"}
-  {ticker: 'TMUS', name: "T-Mobile US", price: "135.05"}
-  {ticker: 'CSCO', name: "Cisco Systems Inc", price: "40.56"}
-  {ticker: 'BMY', name: "Bristol-Meyers Squibb", price: "71.92"}
+  {ticker: 'MSFT', name: "Microsoft", last_price: "148.00"},
+  {ticker: 'APP', name: "Apple", last_price: "141.44"},
+  {ticker: 'GOOG', name: "Google", last_price: "97.49"},
+  {ticker: 'AMZN', name: "Amazon", last_price: "115.80"},
+  {ticker: 'TSLA', name: "Tesla", last_price: "271.13"},
+  {ticker: 'BRK', name: "Berkshire Hathaway", last_price: "270.39"},
+  {ticker: 'UNH', name: "United Health Group", last_price: "511.94"},
+  {ticker: 'JNJ', name: "Johnson and Johnson", last_price: "164.96"},
+  {ticker: 'V', name: "Visa Inc", last_price: "180.52"},
+  {ticker: 'META', name: "Meta Platforms", last_price: "138.76"},
+  {ticker: 'XOM', name: "Exxon Mobile", last_price: "88.37"},
+  {ticker: 'TSM', name: "Taiwan Semiconductor Manufacturing Company", last_price: "69.16"},
+  {ticker: 'WMT', name: "Walmart", last_price: "131.40"},
+  {ticker: 'JPM', name: "JP Morgan Chase", last_price: "105.63"},
+  {ticker: 'LLY', name: "Eli Lilly and Company", last_price: "326.28"},
+  {ticker: 'NVDA', name: "NVIDIA Corporation", last_price: "124.38"},
+  {ticker: 'PG', name: "Proctor & Gamble Company", last_price: "127.77"},
+  {ticker: 'HD', name: "Home Depot Inc", last_price: "280.43"},
+  {ticker: 'CVX', name: "Chervon Corporation", last_price: "144.39"},
+  {ticker: 'MA', name: "MasterCard Incorporated", last_price: "288.65"},
+  {ticker: 'PFE', name: "Pfizer Inc", last_price: "44.45"},
+  {ticker: 'BAC', name: "Bank Of America", last_price: "30.57"},
+  {ticker: 'KO', name: "Coca-Cola Company", last_price: "56.41"},
+  {ticker: 'PEP', name: "PepsiCo Inc", last_price: "165.73"},
+  {ticker: 'NVO', name: "Novo Nordisk", last_price: "100.22"},
+  {ticker: 'MRK', name: "Merck & Company Inc", last_price: "86.86"},
+  {ticker: 'BABA', name: "Alibaba Group Holding", last_price: "80.90"},
+  {ticker: 'COST', name: "Costco", last_price: "480.65"},
+  {ticker: 'TMO', name: "Thermo Fisher", last_price: "516.89"},
+  {ticker: 'DHR', name: "Danaher Corporation", last_price: "262.46"},
+  {ticker: 'AVGO', name: "Broadcom Inc", last_price: "451.99"},
+  {ticker: 'TM', name: "Toyota Motor", last_price: "131.23"},
+  {ticker: 'SHEL', name: "Royal Dutch Shell", last_price: "49.81"},
+  {ticker: 'DIS', name: "Walt Disney Comp", last_price: "96.42"},
+  {ticker: 'ABT', name: "Abott Lab", last_price: "98.79"},
+  {ticker: 'MCD', name: "McDonalds Corporation", last_price: "232.28"},
+  {ticker: 'AZN', name: "AstraZeneca PLC", last_price: "55.10"},
+  {ticker: 'TMUS', name: "T-Mobile US", last_price: "135.05"},
+  {ticker: 'CSCO', name: "Cisco Systems Inc", last_price: "40.56"},
+  {ticker: 'BMY', name: "Bristol-Meyers Squibb", last_price: "71.92"}
 ])
